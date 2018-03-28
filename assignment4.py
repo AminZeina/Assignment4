a8df97837d6787a34ca31e72780e9f7e145cd3cd
Amin Zeina <amin.zeina@ocsbstudent.ca>
2018-03-28 14:31:20 -0400

    Initial commit

---
+++ assignment4.py
@@ -0,0 +1,46 @@
+# Created by Amin Zeina
+# Created on Mar 2018
+
+print('Assignment 4 by Amin Zeina')
+
+input('Press "Enter" to view Part 1.')
+print('''
+The career I chose is software engineering. Software engineers design and build many 
+different programs like operating systems, network control systems and video games, 
+as well as other things. The education required is a minimum of 4 years in university
+and a major in computer science. There is a wide outlook for this career since there 
+are many different things software engineers can do. The one certain thing would be 
+4 years of university. The average salary in the USA in 2014 was $106 050 per year. 
+I am very interested in this career because I enjoy programming and like that there 
+are many different types of programs software engineers are involved with.
+''')
+
+input('Press "Enter" to view Part 2.')
+print('''
+The educational institution I found is the University of Waterloo. Admission 
+requirements are advanced functions, calculus and vectors, chemistry, physics and 
+English. In the first year, I would take multiple math programs for engineering, 
+physics of electrical engineering, programming, and methods of software engineering. 
+They recommend experience in developing programs. The program is 5 years long.
+''')
+
+input('Press "Enter" to view Part 3.')
+print('''
+I feel like being a software engineer will help the common good because I will 
+hopefully be working on multiple different programs that many people use every day. 
+These programs can be things people rely on or can be something important that changes
+someone’s life. It will help me set goals and priorities because software engineers 
+are very busy therefore I would have to balance my time well and plan when I will 
+finish working on something instead of doing something in my personal life. 
+''')
+
+input('Press "Enter" to view References.')
+print('''
+“Software Engineering at Waterloo.” University of Waterloo, 
+https://uwaterloo.ca/future-students/programs/software-engineering
+
+“Software Engineer.” ComputerScience.org, 2018, 
+https://www.computerscience.org/careers/software-engineer/
+''')
+
+input('Program End.')
